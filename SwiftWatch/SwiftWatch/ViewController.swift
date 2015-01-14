392f7cae4342d5de41cfefb5147c151692242fbc
Jagaa <jagaa1103@gmail.com>
2015-01-14 12:10:43 +0900

    first step
    It’s my first step for Apple Watch app:)

---
+++ SwiftWatch/SwiftWatch/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftWatch
+//
+//  Created by Jagaa on 1/14/15.
+//  Copyright (c) 2015 smartmongol. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
